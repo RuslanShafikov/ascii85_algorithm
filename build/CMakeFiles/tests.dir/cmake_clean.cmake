4ee3fe62346f6f29ce638f3e9a8aa5d4ea386e5d
RuslanShafikov <st112650@student.spbu.ru>
2025-04-17 21:52:13 +0300

    CI adding

---
+++ build/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/link.d"
+  "CMakeFiles/tests.dir/coder.cpp.o"
+  "CMakeFiles/tests.dir/coder.cpp.o.d"
+  "CMakeFiles/tests.dir/test/main_test.cpp.o"
+  "CMakeFiles/tests.dir/test/main_test.cpp.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ruslan/CLionProjects/ascii85_algorithm
+# Build directory: /home/ruslan/CLionProjects/ascii85_algorithm/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[MyTest]=] "/home/ruslan/CLionProjects/ascii85_algorithm/cmake-build-debug/tests")
+set_tests_properties([=[MyTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/ruslan/CLionProjects/ascii85_algorithm/CMakeLists.txt;29;add_test;/home/ruslan/CLionProjects/ascii85_algorithm/CMakeLists.txt;0;")
